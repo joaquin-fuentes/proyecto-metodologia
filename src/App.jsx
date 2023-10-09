083462d30ff9471b629ae046a6bb0356520c1fa8
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-10-09 18:36:30 -0300

    primer commit, estructura principal del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Pagina principal</h2>
+    </>
+  )
+}
+
+export default App
